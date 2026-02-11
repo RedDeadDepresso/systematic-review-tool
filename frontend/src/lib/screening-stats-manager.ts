bc7e2c8159596f661c10ac667dc864084b52719b
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2026-02-11 02:52:26 +0000

    fix

--- frontend/src/lib/screening-stats-manager.ts
+++ frontend/src/lib/screening-stats-manager.ts
@@ -46,6 +46,7 @@ class ScreeningStatsManager {
 
     this.currentReviewId = reviewId;
     const token = localStorage.getItem('access_token');
+    console.log(VITE_WS_URL);
     const wsUrl = `${VITE_WS_URL}/screening-stats/${reviewId}/?token=${token}`;
 
     console.log('Connecting to screening stats:', reviewId);
