4ea82ab6792c9a09b767873896a1362265384ca2
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2026-02-11 00:59:58 +0000

    fix(backend): settings in backend/asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,3 +1,5 @@
+# ruff: noqa: E402
+
 """
 ASGI config for backend project.
 
@@ -12,14 +14,16 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 import api.routing
 from api.middleware import JWTAuthMiddleware
 
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
