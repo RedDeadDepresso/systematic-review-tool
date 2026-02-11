7d36b9c765a59b9f4d8adbeafd5d138feb4cbf24
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2026-02-11 00:38:48 +0000

    fix(backend): settings in backend/asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,14 +9,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import api.routing
 from api.middleware import JWTAuthMiddleware
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 django_asgi_app = get_asgi_application()
 
