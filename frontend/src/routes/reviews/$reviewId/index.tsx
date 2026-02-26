56fea3e568d83817fabe82adba5831ad248cc4d6
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2026-02-26 14:42:14 +0000

    fix(frontend): update file upload references dialog to support multiple formats

--- frontend/src/routes/reviews/$reviewId/index.tsx
+++ frontend/src/routes/reviews/$reviewId/index.tsx
@@ -169,10 +169,16 @@ function ReviewPage() {
                           open={openUploadDialog}
                           onOpenChange={setOpenUploadDialog}
                           title="Upload References"
-                          description="Add references to the review"
-                          acceptedFormats=".bib,application/x-bibtex"
-                          acceptedMimeTypes={['application/x-bibtex']}
-                          fileTypeLabel="BibTeX"
+                          description="Add references to the review (BibTeX, RIS, or EndNote XML format)"
+                          acceptedFormats=".bib,.ris,.xml"
+                          acceptedMimeTypes={[
+                            'application/x-bibtex',
+                            'application/x-research-info-systems',
+                            'text/xml',
+                            'application/xml',
+                            'text/plain',
+                          ]}
+                          fileTypeLabel="BibTeX/RIS/EndNote XML"
                           onUpload={handleUploadReferences}
                         />
                         <Button
