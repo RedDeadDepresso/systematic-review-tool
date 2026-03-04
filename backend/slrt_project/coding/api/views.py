042ca69ee7a6d151454668f47708d694f6532d24
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2026-03-04 22:14:54 +0000

    fix(backend): allow members to share coding between each other

--- backend/slrt_project/coding/api/views.py
+++ backend/slrt_project/coding/api/views.py
@@ -1,7 +1,8 @@
 from django_filters import rest_framework as filters
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from slrt_project.coding.api.serializers import (
     CodeSerializer,
@@ -13,7 +14,37 @@ from slrt_project.permissions import Permission, check_permission
 from slrt_project.reviews.models import Review, ReviewMember
 
 
-class CodeViewSet(viewsets.ModelViewSet):
+class CodingMixin:
+    def get_object(self):
+        obj = super().get_object()
+        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, obj.review)
+        return obj
+
+    def list(self, request, *args, **kwargs):
+        review_id = request.query_params.get("review")
+
+        if not review_id:
+            return Response(
+                {"error": "review is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        review = get_object_or_404(Review, pk=review_id)
+        check_permission(Permission.ACCESS_REVIEW, request.user, review)
+        return super().list(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        review_id = self.request.data.get("review")
+        if not review_id:
+            raise serializer.ValidationError("Review is required")
+
+        review = get_object_or_404(Review, pk=review_id)
+
+        member = get_object_or_404(ReviewMember, review=review, user=self.request.user)
+        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, review)
+
+        serializer.save(member=member)
+
+
+class CodeViewSet(CodingMixin, viewsets.ModelViewSet):
     """Owner and collaborator can create/update/delete codes"""
 
     serializer_class = CodeSerializer
@@ -22,104 +53,24 @@ class CodeViewSet(viewsets.ModelViewSet):
     filterset_fields = ["review"]
 
     def get_queryset(self):
-        user = self.request.user
-        return Code.objects.filter(member__user=user).select_related("reference")
-
-    def perform_create(self, serializer):
-        review_id = self.request.data.get("review")
-        if not review_id:
-            raise serializer.ValidationError("Review is required")
-
-        review = get_object_or_404(Review, pk=review_id)
-
-        member = get_object_or_404(ReviewMember, review=review, user=self.request.user)
-        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, review)
-
-        serializer.save(member=member)
-
-    def perform_update(self, serializer):
-        code = self.get_object()
-        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, code.review)
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        check_permission(
-            Permission.MODIFY_THEMES_CODES, self.request.user, instance.review
-        )
-        instance.delete()
+        return Code.objects.select_related("reference")
 
 
-class SubThemeViewSet(viewsets.ModelViewSet):
+class SubThemeViewSet(CodingMixin, viewsets.ModelViewSet):
     """Owner and collaborator can create/update/delete subthemes"""
 
     serializer_class = SubThemeSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.DjangoFilterBackend]
     filterset_fields = ["review"]
-
-    def get_queryset(self):
-        user = self.request.user
-        return SubTheme.objects.filter(member__user=user)
-
-    def perform_create(self, serializer):
-        review_id = self.request.data.get("review")
-        if not review_id:
-            raise serializer.ValidationError("Review is required")
-
-        review = get_object_or_404(Review, pk=review_id)
-
-        member = get_object_or_404(ReviewMember, review=review, user=self.request.user)
-        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, review)
-
-        serializer.save(member=member)
-
-    def perform_update(self, serializer):
-        subtheme = self.get_object()
-        check_permission(
-            Permission.MODIFY_THEMES_CODES, self.request.user, subtheme.review
-        )
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        check_permission(
-            Permission.MODIFY_THEMES_CODES, self.request.user, instance.review
-        )
-        instance.delete()
+    queryset = SubTheme.objects.all()
 
 
-class MainThemeViewSet(viewsets.ModelViewSet):
+class MainThemeViewSet(CodingMixin, viewsets.ModelViewSet):
     """Owner and collaborator can create/update/delete themes"""
 
     serializer_class = MainThemeSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.DjangoFilterBackend]
     filterset_fields = ["review"]
-
-    def get_queryset(self):
-        user = self.request.user
-        return MainTheme.objects.filter(member__user=user)
-
-    def perform_create(self, serializer):
-        review_id = self.request.data.get("review")
-        if not review_id:
-            raise serializer.ValidationError("Review is required")
-
-        review = get_object_or_404(Review, pk=review_id)
-
-        member = get_object_or_404(ReviewMember, review=review, user=self.request.user)
-        check_permission(Permission.MODIFY_THEMES_CODES, self.request.user, review)
-
-        serializer.save(member=member)
-
-    def perform_update(self, serializer):
-        theme = self.get_object()
-        check_permission(
-            Permission.MODIFY_THEMES_CODES, self.request.user, theme.review
-        )
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        check_permission(
-            Permission.MODIFY_THEMES_CODES, self.request.user, instance.review
-        )
-        instance.delete()
+    queryset = MainTheme.objects.all()
